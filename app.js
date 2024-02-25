7beb9951edbde73d778571f662a471d4b19cca48
Utsav Kashichhwa <utsavk0112@gmail.com>
2024-02-25 11:40:10 +1100

    Add image submission endpoints

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 const express = require("express");
 const cors = require("cors");
+const moment = require("moment-timezone");
+require("dotenv").config();
+const process = require("process");
 
 const app = express();
 app.use(cors());
+app.use(express.json({ limit: "10mb" }));
 
 const {
-    getCodesAndWinners,
-    writeCodesAndWinners,
+    getGoldenTicketsSheet,
+    writeGoldenTicketsSheet,
+    getImageSubmissionSheet,
+    writeImageSubmissionSheet,
+    saveImageDataToDrive,
 } = require("./googleFunctions.js");
 
 PORT = 3000;
@@ -16,9 +23,9 @@ app.get("/submit", async (request, response) => {
     const users_name = request.query.name;
     const users_rollClass = request.query.rollClass;
 
-    currentSheetsData = await getCodesAndWinners();
+    currentSheetsData = await getGoldenTicketsSheet();
 
-    const codes = currentSheetsData.map((a) => a[0]);
+    const codes = currentSheetsData.map((a) => a[3]);
 
     if (!codes.includes(users_code)) {
         response.json({
@@ -30,14 +37,10 @@ app.get("/submit", async (request, response) => {
 
     const code_index = codes.indexOf(users_code);
 
-    console.log(currentSheetsData[code_index]);
-
-    console.log(currentSheetsData[code_index][1]);
-
     if (
         !(
-            currentSheetsData[code_index][1] == "" ||
-            currentSheetsData[code_index][1] == undefined
+            currentSheetsData[code_index][4] == "" ||
+            currentSheetsData[code_index][4] == undefined
         )
     ) {
         response.json({
@@ -47,33 +50,158 @@ app.get("/submit", async (request, response) => {
         return;
     }
 
-    currentSheetsData[code_index][1] = users_name;
-    currentSheetsData[code_index][2] = users_rollClass;
-    writeCodesAndWinners(currentSheetsData);
+    currentSheetsData[code_index][4] = users_name;
+    currentSheetsData[code_index][5] = users_rollClass;
+    writeGoldenTicketsSheet(currentSheetsData);
 
     response.json({
         status: "Success",
         message: `Congratulations! You were the first person to guess this code. 🎉
         
-        You have won ${currentSheetsData[code_index][3]}. 
+        You have won ${currentSheetsData[code_index][6]}. 
         
-        As a bonus challenge, ${currentSheetsData[code_index][4]}!`,
+        As a bonus challenge, ${currentSheetsData[code_index][7]}!`,
     });
 });
 
+app.post("/submit-image", async (request, response) => {
+    const users_name = request.query.name;
+    const users_rollClass = request.query.rollClass;
+    const users_code = request.query.code;
+    const users_image = request.body.image;
+
+    const image_data_url = await saveImageDataToDrive(users_image);
+
+    let golden_ticket_id;
+
+    switch (users_code) {
+        case "Chessboard":
+            golden_ticket_id = "10";
+            break;
+        case "Triangle":
+            golden_ticket_id = "11";
+            break;
+        default:
+            response.status(400).send({
+                message: "Not image submission",
+            });
+            return;
+    }
+
+    let submissionData = await getImageSubmissionSheet();
+
+    submissionData = submissionData == undefined ? [] : submissionData;
+
+    const id = submissionData.length + 1;
+    const sydneyTime = moment().tz("Australia/Sydney");
+    const formattedDate = sydneyTime.format("YYYY-MM-DD HH:mm:ss");
+
+    const new_row = [
+        id,
+        golden_ticket_id,
+        users_name,
+        users_rollClass,
+        image_data_url,
+        formattedDate,
+        false,
+    ];
+
+    submissionData.push(new_row);
+
+    writeImageSubmissionSheet(submissionData);
+
+    response.json({
+        status: "Uploaded Solution",
+        message:
+            "Your image has been uploaded! Our execs will verify your solution soon. If your answer is correct, and you were the first solver for this challenge, your name will appear in the Submitted Challenges section below. Check back soon!",
+    });
+});
+
+app.get("/get-unverified-submissions", async (request, response) => {
+    const userAdminCode = request.query.adminCode;
+    const actualAdminCode = process.env.ADMIN_CODE;
+
+    if (userAdminCode != actualAdminCode) {
+        response.status(400).send({
+            message: "Incorrect Admin Code",
+        });
+        return;
+    }
+    let submissionData = await getImageSubmissionSheet();
+
+    submissionData = submissionData == undefined ? [] : submissionData;
+
+    let verified_ids = [];
+
+    submissionData.forEach((row) => {
+        if (row[6] == "TRUE" && !verified_ids.includes(row[1])) {
+            verified_ids.push(row[1]);
+        }
+    });
+
+    const unverified_rows = submissionData.filter(
+        (row) => !verified_ids.includes(row[1])
+    );
+
+    response.json(
+        unverified_rows.map((row) => {
+            return {
+                id: row[0],
+                goldenTicketId: row[1],
+                name: row[2],
+                rollClass: row[3],
+                image: row[4],
+                timestamp: row[5],
+                isAccepted: row[6],
+            };
+        })
+    );
+});
+
+app.get("/verify-submission", async (request, response) => {
+    const userAdminCode = request.query.adminCode;
+    const actualAdminCode = process.env.ADMIN_CODE;
+
+    if (userAdminCode != actualAdminCode) {
+        response.status(400).send({
+            message: "Incorrect Admin Code",
+        });
+        return;
+    }
+
+    let submissionData = await getImageSubmissionSheet();
+    submissionData = submissionData == undefined ? [] : submissionData;
+
+    const id = request.query.id;
+
+    if (!submissionData.map((row) => row[0]).includes(id)) {
+        response.status(400).send({
+            message: "Non-existent submission",
+        });
+        return;
+    }
+
+    submissionData[parseInt(id) - 1][6] = "TRUE";
+
+    await writeImageSubmissionSheet(submissionData);
+    response.json({ status: "Success" });
+});
+
 app.get("/get-submitted-code-information", async (request, response) => {
-    const currentSheetsData = await getCodesAndWinners();
+    const currentSheetsData = await getGoldenTicketsSheet();
     const submittedRows = currentSheetsData.filter(
-        (row) => !(row[1] == "" || row[1] == undefined)
+        (row) => !(row[4] == "" || row[4] == undefined)
     );
 
     const formattedData = submittedRows.map((row) => {
         return {
-            code: row[0],
-            winner: row[1],
-            class: row[2],
-            prize: row[3],
-            challenge: row[4],
+            id: row[0],
+            challengeName: row[2],
+            solution: row[3],
+            winner: row[4],
+            rollClass: row[5],
+            prize: row[6],
+            challenge: row[7],
         };
     });
 
--- googleFunctions.js
+++ googleFunctions.js
@@ -4,7 +4,10 @@ const path = require("path");
 const process = require("process");
 const { google } = require("googleapis");
 
-const SCOPES = ["https://www.googleapis.com/auth/spreadsheets"];
+const SCOPES = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive",
+];
 
 const KEYFILEPATH = path.join(process.cwd(), "credentials.json");
 
@@ -21,25 +24,23 @@ const authClient = new google.auth.GoogleAuth({
 
 async function getSheetsData(
     range,
+    sheetTitle = "Golden Ticket Datasheet",
+
     id = "14SflwbRknYyWZfTnvgYuZHFcivcHHIJdaMYclhIJEiM"
 ) {
     const sheetsService = google.sheets({ version: "v4", auth: authClient });
     const response = await sheetsService.spreadsheets.values.get({
         spreadsheetId: id,
-        range: range,
+        range: `${sheetTitle}!${range}`,
     });
 
     return response.data.values;
 }
 
-async function getCodesAndWinners() {
-    const sheet_data = await getSheetsData("C2:G11");
-    return sheet_data;
-}
-
 async function writeData(
     values,
     range,
+    sheetTitle = "Golden Ticket Datasheet",
     id = "14SflwbRknYyWZfTnvgYuZHFcivcHHIJdaMYclhIJEiM"
 ) {
     const resource = {
@@ -49,17 +50,71 @@ async function writeData(
     const sheetsService = google.sheets({ version: "v4", auth: authClient });
     const response = await sheetsService.spreadsheets.values.update({
         spreadsheetId: id,
-        range,
+        range: `${sheetTitle}!${range}`,
         valueInputOption: "RAW",
         resource,
     });
 }
 
-async function writeCodesAndWinners(values) {
-    writeData(values, "C2:G11");
+async function getGoldenTicketsSheet() {
+    const sheet_data = await getSheetsData("A2:H11");
+    return sheet_data;
+}
+
+async function writeGoldenTicketsSheet(values) {
+    writeData(values, "A2:G11");
+}
+
+async function getImageSubmissionSheet() {
+    const sheet_data = await getSheetsData(
+        "A2:G12",
+        (sheetTitle = "Image Submissions")
+    );
+    return sheet_data;
+}
+
+async function writeImageSubmissionSheet(values) {
+    writeData(values, "A2:H12", (sheetTitle = "Image Submissions"));
+}
+
+async function saveImageDataToDrive(base64String) {
+    const drive = google.drive({ version: "v3", auth: authClient });
+
+    const fileMetadata = {
+        name: `${Math.floor(Math.random() * 10000000)}.txt`,
+        parents: ["1CTmtWQUu2AMth6-b_y7Akfacmnivzuls"],
+        mimeType: "text/plain",
+        uploadType: "media",
+    };
+
+    image_data_url = await new Promise((resolve, reject) => {
+        drive.files.create(
+            {
+                resource: fileMetadata,
+                media: {
+                    mimeType: "text/plain",
+                    body: base64String,
+                },
+
+                fields: "id, webViewLink", // Request fields 'id' and 'webViewLink'
+            },
+            (err, file) => {
+                if (err) {
+                    reject(err);
+                }
+
+                resolve(file.data.webViewLink);
+            }
+        );
+    });
+
+    return image_data_url;
 }
 
 module.exports = {
-    getCodesAndWinners,
-    writeCodesAndWinners,
+    getGoldenTicketsSheet,
+    writeGoldenTicketsSheet,
+    getImageSubmissionSheet,
+    writeImageSubmissionSheet,
+    saveImageDataToDrive,
 };
